a489e39b72cce7257677eed067ce6421a6c1f6fd
Ramon Muñoz <ramonm2811@gmail.com>
2021-10-05 07:51:40 -0300

    third commit

--- src/components/CounterApp.js
+++ src/components/CounterApp.js
@@ -5,7 +5,7 @@ const CounterApp = () => {
     <div>
       <h1>Counter {0}</h1>
       <hr />
-      <button className="btn btn-primary">+1</button>
+      <button className="btn btn-primary"> +1</button>
     </div>
   );
 };
