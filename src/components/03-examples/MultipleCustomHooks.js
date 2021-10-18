7341c28d82fb379be3d783a1cdb77206843e75f2
Ramon Muñoz <ramonm2811@gmail.com>
2021-10-18 09:52:39 -0300

    keep working

--- src/components/03-examples/MultipleCustomHooks.js
+++ src/components/03-examples/MultipleCustomHooks.js
@@ -5,20 +5,23 @@ import "../02-useEffect/effect.css";
 const MultipleCustomHooks = () => {
   const state = useFetch(`https://www.breakingbadapi.com/api/quotes/1`);
   const { loading, data } = state;
-  // console.log(data[0].quote);
-  console.log();
+  //Se hace esta validación debido que al principio data == null, eso hace que data[0] sea igual a null[0], generando un error.
+  if (data !== null) {
+    const { author, quote } = data[0];
+  }
+
   return (
     <div>
-      <h1>Custom Hooks</h1>
+      <h1>Breaking Bad Quotes</h1>
       <hr />
       {loading ? (
         <div className="alert alert-primary text-center" role="alert">
-          Loading
+          Loading...
         </div>
       ) : (
         <blockquote className="text-end">
-          Message
-          <footer>-Ramon</footer>
+          {quote}
+          <footer className="blockquote-footer">{author}</footer>
         </blockquote>
       )}
     </div>
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 /* Hook que se encarga de hacer la petición Fetch a la API correspondiente y retorna 
-un State con la data de la petición, el error y loading. */
+un State tipo objeto con la data de la petición, el error y loading. */
 const useFetch = (url) => {
   const [state, setState] = useState({
     data: null,
